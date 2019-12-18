8fd7804a5855c636a8c3a0603719fa9660b19c31
ethemgur <iethemgur@gmail.com>
2019-12-18 18:23:51 +0100

    Add CK as index

--- src/DML_stmts_Group19.sql
+++ src/DML_stmts_Group19.sql
@@ -1,3 +1,6 @@
 UPDATE Assignment
 SET DeadlineDate = "2.10.2018"
 WHERE AssignmentID = 7001;
+
+DELETE FROM Project
+WHERE Status = 0;
--- src/create_index.sql
+++ src/create_index.sql
@@ -1,23 +1,11 @@
-CREATE INDEX idx_InstructorID
-ON Instructor (InstructorID);
+CREATE INDEX idx_InstructorEmail
+ON Instructor (InstructorEmail);
 
-CREATE INDEX idx_StudentID
-ON Student (StudentID);
+CREATE INDEX idx_StudentEmail
+ON Student (StudentEmail);
 
-CREATE INDEX idx_ContactID
-ON CompanyContact (ContactID);
+CREATE INDEX idx_ContactEmail
+ON CompanyContact (Email);
 
-CREATE INDEX idx_GroupID
-ON Groups (GroupID);
-
-CREATE INDEX idx_CourseID
-ON Course (CourseID);
-
-CREATE INDEX idx_AssignmentID
-ON Assignment (AssignmentID);
-
-CREATE INDEX idx_SubmissionID
-ON Submission (SubmissionID);
-
-CREATE INDEX idx_ProjectID
-ON Project (ProjectID);
+CREATE INDEX idx_ContactPhone
+ON CompanyContact (Phone);
